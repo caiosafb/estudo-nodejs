18f71e6e3af86ef28c52b5ec86de0fa1109ee6d5
Caio Sá <caiobarcellos16@gmail.com>
2023-08-23 16:58:28 -0300

    Criando a home do projeto

--- index.js
+++ index.js
@@ -1,74 +1,75 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-const session = require('express-session')
-const FileStore = require('session-file-store')(session)
-const flash = require('express-flash')
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const flash = require("express-flash");
 
+const app = express();
 
-const app = express()
+const conn = require("./db/conn");
 
-const conn = require('./db/conn')
+// Models
+const Tought = require("./models/Tought");
 
-//Models - chamando model para ser criada tabela quando iniciar o projeto 
-    const Tought = require('./models/Tought')
-    const User = require('./models/User')
+// routes
+const toughtsRoutes = require("./routes/toughtsRoutes");
+const ToughController = require("./controllers/ToughtController");
 
-//Configuração handlebars - template engine
-    app.engine('handlebars', exphbs())
-    app.set('view engine', 'handlebars')
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
-//Receber resposta do body -> conseguir extrair quando vir requisição de formulário
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-    app.use(
-    express.urlencoded({
-        extended: true
-    })
-    )
+app.use(express.json());
 
-//Receber dados em JSON
-    app.use(express.json())
+//session middleware
+app.use(
+  session({
+    name: 'session',
+    secret: 'nosso_secret',
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () {},
+      path: require('path').join(require('os').tmpdir(), 'sessions'),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 3600000,
+      expires: new Date(Date.now() + 3600000),
+      httpOnly: true,
+    },
+  }),
+)
 
-//Session middleware - onde o express vai salvar as sessões
-    app.use(
-        session({
-            name: "session",
-            secret: "nosso_secret",
-            resave: false,
-            saveUninitialized: false,
-            store: new FileStore({
-                logFn: function() {},
-                path: require('path').join(require('os').tmpdir(), 'sessions'),
-            }),
-            cookie: {
-                secure: false,
-                maxAge: 360000,
-                expires: new Date(Date.now() + 36000),
-                httpOnly: true
-            }
-        }),
-    )
+// flash messages
+app.use(flash());
 
-//Flash messages - mensagens do status do sistema - quando fazemos uma operação com db recebendo feedback
-    app.use(flash())
+app.use(express.static("public"));
 
-//Arquivos públicos - assets
-    app.use(express.static('public'))
+// set session to res
+app.use((req, res, next) => {
+  // console.log(req.session)
+  console.log(req.session.userid);
 
-//Set session to res -> Salvar a sessão na reposta
-    
-    app.use((req, res, next) => {
-        
-        if(req.session.userid) {
-            res.locals.session - req.session
-        }
+  if (req.session.userid) {
+    res.locals.session = req.session;
+  }
 
-        next()
-    })
-conn 
-    // .sync({force: true})
-    .sync()
-    .then(() => {
-        app.listen(3000)
-    })
-     .catch((err) => console.log(err))
+  next();
+});
 
+app.use("/toughts", toughtsRoutes);
+
+app.get("/", ToughController.showToughts);
+
+conn
+  .sync()
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
