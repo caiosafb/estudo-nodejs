d8af5b969250cdfddee1ff75ec22fd91b300a7b5
Caio Sá <caiobarcellos16@gmail.com>
2023-08-28 16:48:15 -0300

    CSS da dashboard

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -12,7 +12,7 @@ module.exports = class ToughtController {
 
         const user = await User.findOne({
             where: {
-                id: userId
+                id: userId,
             },
             include: Tought, 
             plain: true,
